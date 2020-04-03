8aac27677f37b64c816ba5020976281998e5a64c
magicallay <pierre-marie.airiau@hotmail.fr>
2020-04-03 21:39:36 +0200

    add postgresql database local or docker + db init + first endpoints in api to interact with games and players

---
+++ api/src/controllers/db.js
@@ -0,0 +1,72 @@
+const uuidv4 = require('uuid').v4
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'api',
+    password: 'example',
+    port: 5432,
+})
+
+const getGames = (request, response) => {
+    const query = 'SELECT * FROM games ORDER BY id ASC'
+    pool.query(query, (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getPlayers = (request, response) => {
+    const game_id = request.body.game_id
+    const query = `
+    SELECT id,
+           player_name
+    FROM players
+    WHERE game_id = ${game_id}
+    ORDER BY id ASC`
+    pool.query(query, (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const createGame = (request, response) => {
+    const game_type = request.body.game_type
+    const query = `
+    INSERT INTO games
+            (game_type)
+    VALUES  ('${game_type}')`
+    pool.query(query, (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(results.insertId)
+    })
+}
+
+const createPlayer = (request, response) => {
+    const name = request.body.name
+    const game_id = request.body.game_id
+    const player_token = uuidv4()
+    const query = `
+    INSERT INTO players
+            (game_id, player_name, player_token)
+    VALUES  (${game_id}, '${name}', '${player_token}')`
+    pool.query(query, (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(player_token)
+    })
+}
+
+module.exports = {
+    getGames,
+    getPlayers,
+    createGame,
+    createPlayer,
+}
---
+++ db/README.md
@@ -0,0 +1,18 @@
+
+# Up postgresql database
+
+## On windows (local run)
+
+https://www.postgresql.org/download/windows/
+
+## With Docker
+
+up database: `docker-compose up`
+
+# Connect postgresql database
+
+connect database (once started): `psql -h localhost -p 5432 -U postgres`
+
+# Init postgres database
+
+run in postgres the `init-db.sql`script
